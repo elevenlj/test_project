f59e8b4b9578ee25bc463f142e30e6454b60f0c1
eleven <abc@com>
2020-05-05 10:27:47 +0800

    项目经理创建好项目了

---
+++ src/main/java/com/eleven/test_project/TestController.java
@@ -0,0 +1,18 @@
+package com.eleven.test_project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: lijun
+ * @Date: 2020/5/5
+ * @Description:TODO
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @RequestMapping("method1")
+    public String method1(){
+        return "method1";
+    }
+}
